6260aaf3bc0bbb039c6ea50ca5b3d965c5a6f170
otaviojunqueira <otaviocostajunqueira326@gmail.com>
2025-06-05 19:44:16 -0300

    criacao da pasta dist

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    sourcemap: false,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
